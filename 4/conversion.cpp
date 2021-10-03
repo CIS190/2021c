4993e3221fe7f5b3cdb6c4b4fef5627c7cd026de
Paul He <paulhe@cis.upenn.edu>
2021-10-03 18:06:13 -0400

    Add lecture 4 notes

---
+++ 4/conversion.cpp
@@ -0,0 +1,18 @@
+class list
+{
+public:
+	// prepend explicit to disable the implicit conversion
+    list(int size) {}
+    list(int first, int second) {} // first two elements of the list, or something
+};
+
+void printList(list l)
+{
+    // ...
+}
+
+int main()
+{
+    printList(1); // maybe we expect to print the list with the contents [1]?
+    printList({1, 2});
+}
